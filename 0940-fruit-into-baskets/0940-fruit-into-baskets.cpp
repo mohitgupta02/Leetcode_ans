761ddee33f80ddb1c5a30463af42582119aa27b6
Mohit Gupta <92048287+mohitgupta02@users.noreply.github.com>
2025-06-16 18:06:21 +0530

    Time: 40 ms (58.65%), Space: 75.1 MB (56.41%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -9,17 +9,13 @@ public:
             mpp[fruits[r]]++;
 
             if(mpp.size() > 2){
-                while(mpp.size() > 2){
                     mpp[fruits[l]]--;
                     if(mpp[fruits[l]] ==0){
                         mpp.erase(fruits[l]);
                     }
                     l++;
-                }
             }
-            if(mpp.size() <= 2){
-                maxLen = max(maxLen, r-l+1);
-            }
+            maxLen =max(maxLen, r-l+1);
             r++;
         }
         return maxLen;
