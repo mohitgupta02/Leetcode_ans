e696885193d0d1b74af8e3ab9d5d5467ecbe1b21
Mohit Gupta <92048287+mohitgupta02@users.noreply.github.com>
2025-06-18 02:05:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (21.93%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static vector<int>dp;
+    Solution() {
+        if(dp.empty()) dp.resize(31,-1);
+    }
+    int fib(int n) {
+        if(n<=1) return n;
+        int first,second;
+
+        if(dp[n-1] != -1) first = dp[n-1];
+        else first = fib(n-1);
+
+        if(dp[n-2] != -1) second = dp[n-2];
+        else second = fib(n-2);
+
+        return dp[n] = first + second;
+
+    }
+};
+
+
+vector<int> Solution::dp;
