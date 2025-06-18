bc81c900348acfe947e39c767a9c07efc305393a
Mohit Gupta <92048287+mohitgupta02@users.noreply.github.com>
2025-06-18 08:52:21 +0530

    Time: 0 ms (100%), Space: 10.7 MB (62.72%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-
-    int solve(int i , vector<int>& nums,vector<int>&dp,int n ){
-        if(i > n) return 0;
+    int solve(int i , vector<int>& nums,int n,vector<int>& dp){
+        if(i >= n) return 0;
         if(dp[i] != -1) return dp[i];
-        int steal = nums[i] + solve(i+2,nums,dp,n);
-        int skip = solve(i+1,nums,dp,n);
+        int steal = nums[i] + solve(i+2,nums,n,dp);
+        int skip = solve(i+1,nums,n,dp);
 
         return dp[i] = max(steal,skip);
 
@@ -14,9 +13,9 @@ public:
         int n = nums.size();
         if(n==1) return nums[0];
         vector<int>dp(n+1,-1);
-        int case1 = solve(0,nums,dp,n-2);
-        vector<int>dp1(n+1,-1);
-        int case2 = solve(1,nums,dp1,n-1);
-        return max(case1,case2);
+        int ans1=solve(0,nums,n-1,dp);
+        vector<int>dp2(n+1,-1);
+        int ans2 = solve(1,nums,n,dp2);
+        return max(ans1, ans2);
     }
 };
