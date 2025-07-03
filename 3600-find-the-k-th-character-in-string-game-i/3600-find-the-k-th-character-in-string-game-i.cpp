c0655b94a1d23988606bd7791521e30ae6fe8c9f
Mohit Gupta <92048287+mohitgupta02@users.noreply.github.com>
2025-07-03 18:19:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.62%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s ="a";
-        while(s.size() < k){
-            string t ="";
-            for(int i =0;i<s.size();i++){
-                t += (s[i]+1);
-            }
-            s = s+t;
-        }
-        return s[k-1];
+        int x =__builtin_popcount(k-1);
+        return 'a' + x;
     }
 };
