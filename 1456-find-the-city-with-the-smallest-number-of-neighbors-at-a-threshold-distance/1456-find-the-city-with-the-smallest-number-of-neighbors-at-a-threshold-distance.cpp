d914ae6407111a04f2b594faf3b91f88fd4f822f
Mohit Gupta <92048287+mohitgupta02@users.noreply.github.com>
2025-07-08 12:38:57 +0530

    Time: 30 ms (16.95%), Space: 17.9 MB (24.28%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-
-        for(auto it:edges){
-            dist[it[0]][it[1]]=it[2];
-            dist[it[1]][it[0]]=it[2];
+        vector<pair<int,int>> adj[n];
+        for(auto it:edges)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
-
-        for(int i=0;i<n;i++) dist[i][i]=0;
-        for(int via =0;via<n;via++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(dist[i][via] == INT_MAX || dist[via][j] == INT_MAX) continue;
-                    dist[i][j] = min(dist[i][via]+dist[via][j], dist[i][j]);
-
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+        int cityno,mincitycount=1e9;
+        
+        for(int i=0;i<n;i++)
+        {
+            vector<int> dist(n,1e9);
+            pq.push({0,i});
+            dist[i]=0;
+            while(!pq.empty())
+            {
+                int distance=pq.top().first;
+                int node=pq.top().second;
+                pq.pop();
+                for(auto it:adj[node])
+                {
+                    int adjNode=it.first;
+                    int adjWeight=it.second;
+                    if(distance + adjWeight < dist[adjNode])
+                    {
+                        dist[adjNode] = distance + adjWeight;
+                        pq.push({dist[adjNode],adjNode});
+                    }
                 }
             }
-        }
-        int cntCity =n;
-        int cityNo=-1;
-        for(int city =0;city<n;city++){
-            int count =0;
-            for(int adjCity=0;adjCity<n;adjCity++){
-                if(dist[city][adjCity] <= distanceThreshold){
+            int count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(dist[j]<=distanceThreshold)
                     count++;
-                }
             }
-            if(count <= cntCity){
-                cntCity = count;
-                cityNo = city;
+            if(count<=mincitycount)
+            {
+                mincitycount=count;
+                cityno = i;
             }
         }
-        return cityNo;
-
+        return cityno;
     }
 };
